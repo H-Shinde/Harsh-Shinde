b37f5c4ebae1d12b982ccd95be3411248aba8f23
Harshvardhan Shinde <136096571+H-Shinde@users.noreply.github.com>
2025-08-22 12:00:17 -0400

    Finished HomePage

--- src/App.jsx
+++ src/App.jsx
@@ -2,21 +2,27 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import Header from './components/header'
-
+import Hero from './components/Hero'
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+import { useEffect } from 'react'
 function App() {
-  const [count, setCount] = useState(0)
+  useEffect(()=>{
+    AOS.init({
+      duration: 1500,
+      once: true,
+    })
+  })
 
   return (
     <>
      <main>
-{/* Gradient Image */}
 
-<img className='absolute top-0 right-0 opacity-60 -z-10' src="/gradient.png" alt="" />
+      <img className='absolute top-0 right-0 opacity-60 -z-10' src="/gradient.png" alt="" />
+      <div className='h-0 w-[40rem] absolute top[20%] right-[-5%] shadow-[0_0_900px_20px_#e99b63] -rotate-[30deg] -z-10'></div>
+      <Header/>
+      <Hero/>
 
-{/* Blur */}
-<div className='h-0 w-[40rem] absolute top[20%] right-[-5%] shadow-[0_0_900px_20px_#e99b63] -rotate-[30deg] -z-10'></div>
-
-<Header></Header>
      </main>
     </>
   )
